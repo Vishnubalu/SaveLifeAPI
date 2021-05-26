e89dbdab29f7eba82467212cacc5895f9242e983
vishnubalu <vishnubalu048@gmail.com>
2021-05-26 09:52:08 +0530

    prefinal

--- API/sms.py
+++ API/sms.py
@@ -4,8 +4,8 @@ from twilio.rest import Client
 def send_sms(sms, number):
     try:
         print("inside send sms")
-        account_sid = 'AC0fe82cc5fc37fd70e382fdcb0a259734'
-        auth_token = 'dd1db738c2f8be75a438657ca45a186c'
+        account_sid = 'your SID'
+        auth_token = 'your TOKEN'
         client = Client(account_sid, auth_token)
         print("after auth")
         numb = "+91" + str(number)
